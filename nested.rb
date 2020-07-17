05bad60e083bb4b76426a957756a6f19c8c3679b
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 17:15:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,7 @@ def adding_matz
         }
      }
      
-    programmer_hash = {}
-    program_hash.unshift [:yukihiro_matsumoto]
+    programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
