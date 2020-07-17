934af9d127a81eefb032e61b7c20069055f6a3f1
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 17:47:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,6 +103,12 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     alans_new_info = 
+     programmer_hash[:alan_kay] => {
+       :known_for => "GUI"
+     }
+     
+     programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
