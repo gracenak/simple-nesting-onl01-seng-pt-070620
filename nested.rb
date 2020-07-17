4181b610e686fba52e4cbbd961c025022f820ab0
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-17 17:55:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,10 +103,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
      alans_new_info = ["GUI"]
      programmer_hash[:alan_kay] = {
-       :known_for => ["GUI"]
-        }
+       :known_for => ["GUI"]}
      }
      
      programmer_hash
